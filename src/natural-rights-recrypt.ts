cb596d62886df84e9360efad8170f81083746e43
go1dfish <me@go1dfish.me>
2019-07-14 19:52:48 -0700

    Initial implementation

--- src/natural-rights-recrypt.ts
+++ src/natural-rights-recrypt.ts
@@ -1,4 +1,96 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
-export default class DummyClass {}
+import * as JSON from 'buffer-json'
+
+const encoding = 'hex'
+
+interface KeyPair {
+  readonly privKey: string
+  readonly pubKey: string
+}
+
+export default function recryptApiToNaturalRights(RecryptApi: any) {
+  return {
+    async cryptKeyGen() {
+      const encryptionKeys = RecryptApi.generateKeyPair()
+      const pubX = encryptionKeys.publicKey.x.toString(encoding)
+      const pubY = encryptionKeys.publicKey.y.toString(encoding)
+
+      return {
+        privKey: encryptionKeys.privateKey.toString(encoding) as string,
+        pubKey: `${pubX}.${pubY}`
+      }
+    },
+
+    async cryptTransformKeyGen(fromKeyPair: KeyPair, toPubKey: string, signKeyPair: KeyPair) {
+      const [pubX, pubY] = toPubKey.split('.')
+
+      return JSON.stringify(
+        RecryptApi.generateTransformKey(
+          Buffer.from(fromKeyPair.privKey, encoding),
+          {
+            x: Buffer.from(pubX, encoding),
+            y: Buffer.from(pubY, encoding)
+          },
+          Buffer.from(signKeyPair.privKey, encoding)
+        )
+      ) as string
+    },
+
+    async encrypt(pubKey: string, plaintext: string, signKeyPair: KeyPair) {
+      const [pubX, pubY] = pubKey.split('.')
+      let padded = plaintext
+
+      while (padded.length < 384) padded += ' '
+
+      return JSON.stringify(
+        RecryptApi.encrypt(
+          Buffer.from(padded, 'utf-8'), // TODO: Explicit encoding?
+          {
+            x: Buffer.from(pubX, encoding),
+            y: Buffer.from(pubY, encoding)
+          },
+          Buffer.from(signKeyPair.privKey, encoding)
+        )
+      ) as string
+    },
+
+    async signKeyGen() {
+      const signingKeys = RecryptApi.generateEd25519KeyPair()
+
+      return {
+        privKey: signingKeys.privateKey.toString(encoding) as string,
+        pubKey: signingKeys.publicKey.toString(encoding) as string
+      }
+    },
+
+    async cryptTransform(transformKey: string, ciphertext: string, signKeyPair: KeyPair) {
+      return JSON.stringify(
+        RecryptApi.transform(
+          JSON.parse(ciphertext),
+          JSON.parse(transformKey),
+          Buffer.from(signKeyPair.privKey, encoding)
+        )
+      ) as string
+    },
+
+    async decrypt(keyPair: KeyPair, ciphertext: string) {
+      return RecryptApi.decrypt(JSON.parse(ciphertext), Buffer.from(keyPair.privKey, encoding))
+        .toString('utf-8')
+        .trim() as string
+    },
+
+    async sign(keyPair: KeyPair, text: string) {
+      return RecryptApi.ed25519Sign(
+        Buffer.from(keyPair.privKey, encoding),
+        Buffer.from(text)
+      ).toString(encoding) as string
+    },
+
+    async verify(pubKey: string, signature: string, text: string) {
+      return RecryptApi.ed25519Verify(
+        Buffer.from(pubKey, encoding),
+        Buffer.from(text),
+        Buffer.from(signature, encoding)
+      ) as boolean
+    }
+  }
+}
--- test/natural-rights-recrypt.test.ts
+++ test/natural-rights-recrypt.test.ts
@@ -1,14 +1,41 @@
-import DummyClass from '../src/natural-rights-recrypt'
+import recryptApiToNaturalRights from '../src/natural-rights-recrypt'
 
-/**
- * Dummy test
- */
-describe('Dummy test', () => {
-  it('works if true is truthy', () => {
-    expect(true).toBeTruthy()
+const Recrypt = require('@ironcorelabs/recrypt-node-binding')
+const RecryptApi = new Recrypt.Api256()
+const Primitives = recryptApiToNaturalRights(RecryptApi)
+
+describe('natural rights primitives', () => {
+  it('allows encryption and decryption', async () => {
+    const cryptKeyPair = await Primitives.cryptKeyGen()
+    const signKeyPair = await Primitives.signKeyGen()
+    const plaintext = 'Some plaintext'
+    const ciphertext = await Primitives.encrypt(cryptKeyPair.pubKey, plaintext, signKeyPair)
+    const decrypted = await Primitives.decrypt(cryptKeyPair, ciphertext)
+    expect(decrypted).toEqual(plaintext)
   })
 
-  it('DummyClass is instantiable', () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  it('allows re-encryption and decryption', async () => {
+    const cryptKeyPair = await Primitives.cryptKeyGen()
+    const otherCryptKeyPair = await Primitives.cryptKeyGen()
+    const signKeyPair = await Primitives.signKeyGen()
+    const plaintext = 'Some plaintext'
+    const ciphertext = await Primitives.encrypt(cryptKeyPair.pubKey, plaintext, signKeyPair)
+    const transformKey = await Primitives.cryptTransformKeyGen(
+      cryptKeyPair,
+      otherCryptKeyPair.pubKey,
+      signKeyPair
+    )
+    const transformed = await Primitives.cryptTransform(transformKey, ciphertext, signKeyPair)
+    const decrypted = await Primitives.decrypt(otherCryptKeyPair, transformed)
+    expect(decrypted).toEqual(plaintext)
+  })
+
+  it('supports signatures', async () => {
+    const signKeyPair = await Primitives.signKeyGen()
+    const otherSignKeyPair = await Primitives.signKeyGen()
+    const text = 'Some text to sign'
+    const signature = await Primitives.sign(signKeyPair, text)
+    expect(await Primitives.verify(signKeyPair.pubKey, signature, text)).toEqual(true)
+    expect(await Primitives.verify(otherSignKeyPair.pubKey, signature, text)).toEqual(false)
   })
 })
